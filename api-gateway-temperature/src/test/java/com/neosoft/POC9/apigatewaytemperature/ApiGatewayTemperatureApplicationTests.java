aba74e4193935d055175d2e1fca1f814a1b9ae88
Bhagyashri <bhagyashri.mandlik@neosoftmail.com>
2021-05-28 12:52:00 +0530

    Final commit (API gateway)

---
+++ api-gateway-temperature/src/test/java/com/neosoft/POC9/apigatewaytemperature/ApiGatewayTemperatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neosoft.POC9.apigatewaytemperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayTemperatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
